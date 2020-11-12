94eef907a7a6f54f1d7c27664b7888d73fcf0d54
eslam-fakhry <eslamphakhry@gmail.com>
2020-11-12 17:03:30 +0200

    Initial commit

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'coffee-shop-eslam.us', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'OjONRU0Gk22Me6gVmokHiqt8P1NuSeTY', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
+};
